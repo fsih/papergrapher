237c729d463a6a11ef9dae9d70713baf132e71a8
DD Liu <liudi@media.mit.edu>
2017-06-08 15:45:59 -0400

    merge segment and broad brush into brush

--- js/tools/brush.js
+++ js/tools/brush.js
@@ -28,7 +28,13 @@ pg.tools.broadbrush = function() {
 		// get options from local storage if present
 		options = pg.tools.getLocalOptions(options);
 		tool = new Tool();
-		var lastPoint, secondLastPoint;
+		var lastPoint, secondLastPoint, finalPath, brush;
+		var BROAD = "broadbrush";
+		var SEGMENT = "segmentbrush";
+		// brush size >= threshold use segment brush, else use broadbrush
+		// Segment brush has performance issues at low threshold, but broad brush has weird corners
+		// which are more obvious the bigger it is
+		var THRESHOLD = 10;
 		
 		cc = new Path.Circle({
 			    center: [-10000, -10000],
@@ -37,7 +43,50 @@ pg.tools.broadbrush = function() {
 			});
 		
 		tool.fixedDistance = 1;
+
+
+		tool.onMouseMove = function(event) {
+			cc.remove();
+			cc = new Path.Circle({
+			    center: event.point,
+			    radius: options.brushWidth/2,
+			    fillColor: pg.stylebar.getFillColor()
+			});
+		};
+		
 		tool.onMouseDown = function(event) {
+			if (options.brushWidth < THRESHOLD) {
+				brush = BROAD;
+				this.onBroadMouseDown(event);				
+			} else {
+				brush = SEGMENT;
+				this.onSegmentMouseDown(event);
+			}
+		};
+
+		tool.onMouseDrag = function(event) {
+			if (brush === BROAD) {
+				this.onBroadMouseDrag(event);
+			} else if (brush === SEGMENT) {
+				this.onSegmentMouseDrag(event);
+			} else {
+				console.warn("Brush type does not exist: ", brush);
+			}
+		};
+
+		tool.onMouseUp = function(event) {
+			if (brush === BROAD) {
+				this.onBroadMouseUp(event);
+			} else if (brush === SEGMENT) {
+				this.onSegmentMouseUp(event);
+			} else {
+				console.warn("Brush type does not exist: ", brush);
+			}
+			brush = undefined;
+		};
+
+		// broad brush =======================================================================
+		tool.onBroadMouseDown = function(event) {
 			tool.minDistance = options.brushWidth/4;
 			tool.maxDistance = options.brushWidth;
 			if(event.event.button > 0) return;  // only first mouse button
@@ -48,7 +97,7 @@ pg.tools.broadbrush = function() {
 			lastPoint = secondLastPoint = event.point;
 		};
 		
-		tool.onMouseDrag = function(event) {
+		tool.onBroadMouseDrag = function(event) {
 			if(event.event.button > 0) return;  // only first mouse button
 			
 			cc.position = event.point;
@@ -89,16 +138,7 @@ pg.tools.broadbrush = function() {
 			cc.position = event.point;
 		};
 
-		tool.onMouseMove = function(event) {
-			cc.remove();
-			cc = new Path.Circle({
-			    center: event.point,
-			    radius: options.brushWidth/2,
-			    fillColor: pg.stylebar.getFillColor()
-			});
-		};
-
-		tool.onMouseUp = function(event) {
+		tool.onBroadMouseUp = function(event) {
 			if(event.event.button > 0) return;  // only first mouse button
 			
 			// If the mouse up is at the same point as the mouse drag event then we need
@@ -138,16 +178,117 @@ pg.tools.broadbrush = function() {
 
 			pg.undo.snapshot('broadbrush');
 		};
+
+		// Segment brush ================================================
+		tool.onSegmentMouseDown = function(event) {
+			tool.minDistance = options.brushWidth/4;
+			tool.maxDistance = options.brushWidth;
+			if(event.event.button > 0) return;  // only first mouse button
+			
+			finalPath = new Path.Circle({
+			    center: event.point,
+			    radius: options.brushWidth/2,
+			    fillColor: pg.stylebar.getFillColor()
+			});
+			lastPoint = event.point;
+		};
+		
+		tool.onSegmentMouseDrag = function(event) {
+			if(event.event.button > 0) return;  // only first mouse button
+			
+			cc.position = event.point;
+
+			var step = (event.delta).normalize(options.brushWidth/2);
+			var handleVec = step.clone();
+			handleVec.length = options.brushWidth/2;
+			handleVec.angle += 90;
+
+			path = new Path();
+			path = pg.stylebar.applyActiveToolbarStyle(path);
+			// Add handles to round the end caps
+			path.add(new Segment(lastPoint - step, -handleVec, handleVec));
+			step.angle += 90;
+
+			path.selected = true;
+			path.add(event.lastPoint + step);
+			path.insert(0, event.lastPoint - step);
+			path.add(event.point + step);
+			path.insert(0, event.point - step);
+
+			// Add end cap
+			step.angle -= 90;
+			path.add(new Segment(event.point + step, handleVec, -handleVec));
+			path.closed = true;
+			// The unite function on curved paths does not always work (sometimes deletes half the path)
+			// so we have to flatten.
+			path.flatten(3);
+			
+			lastPoint = event.point;
+			var newPath = finalPath.unite(path);
+			path.remove();
+			finalPath.remove();
+			finalPath = newPath;
+			cc.position = event.point;
+		};
+
+		tool.onSegmentMouseUp = function(event) {
+			if(event.event.button > 0) return;  // only first mouse button
+
+			// TODO: This smoothing tends to cut off large portions of the path! Would like to eventually
+			// add back smoothing, maybe a custom implementation?
+
+			// Smooth the path.
+			//finalPath.simplify(2);
+
+			// reset
+			tool.fixedDistance = 1;
+
+			pg.undo.snapshot('broadbrush');
+
+			// Get all Path items
+			var items = paper.project.getItems({
+			    'class': Path
+			});
+			for (var i = 0; i < items.length; i++) {
+				// Ignore the cursor preview
+				if (items[i] === cc) { continue; }
+				if (!items[i].intersects(finalPath)) { continue; }
+				if (!items[i].getFillColor()) {
+					// Ignore overlapping a hole (need more logic for this; shape with hole has multiple fill colors)
+				} else if (items[i].getFillColor().equals(finalPath.fillColor)) {
+					// Merge same fill color
+					var newPath = finalPath.unite(items[i]);
+					finalPath.remove();
+					items[i].remove();
+					finalPath = newPath;
+				} else {
+					// Erase different color
+					var newPath = items[i].subtract(finalPath);
+					if (newPath.children) {
+					    for (var j = newPath.children.length - 1; j >= 0; j--) {
+						    var child = newPath.children[j];
+						    child.copyAttributes(newPath);
+						    child.fillColor = newPath.fillColor;
+						    child.insertAbove(newPath);
+					    }
+				    	newPath.remove();
+					}
+				    items[i].remove();
+				}
+			}
+			pg.undo.snapshot('broadbrush');
+		};
 		
 		// setup floating tool options panel in the editor
 		pg.toolOptionPanel.setup(options, components, function() {});
 		
 		tool.activate();
 	};
+	//=========================================================
 
 	var deactivateTool = function() {
 		cc.remove();
-	}
+	};
 	
 	return {
 		options: options,
