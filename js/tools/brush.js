648957550e974e41694380c539d1b235ea188ebc
DD Liu <liudi@media.mit.edu>
2017-06-09 16:28:58 -0400

    add merging to both brushes. Remove erase

--- js/tools/brush.js
+++ js/tools/brush.js
@@ -4,12 +4,12 @@
 
 pg.tools.registerTool({
 	id: 'broadbrush',
-	name: 'Broad brush'
+	name: 'Brush'
 });
 
 pg.tools.broadbrush = function() {
 	var tool;
-	var path;
+	var finalPath;
 	var cc;
 
 	var options = {
@@ -28,13 +28,13 @@ pg.tools.broadbrush = function() {
 		// get options from local storage if present
 		options = pg.tools.getLocalOptions(options);
 		tool = new Tool();
-		var lastPoint, secondLastPoint, finalPath, brush;
+		var lastPoint, secondLastPoint, brush;
 		var BROAD = "broadbrush";
 		var SEGMENT = "segmentbrush";
 		// brush size >= threshold use segment brush, else use broadbrush
 		// Segment brush has performance issues at low threshold, but broad brush has weird corners
 		// which are more obvious the bigger it is
-		var THRESHOLD = 10;
+		var THRESHOLD = 20;
 		
 		cc = new Path.Circle({
 			    center: [-10000, -10000],
@@ -82,8 +82,51 @@ pg.tools.broadbrush = function() {
 			} else {
 				console.warn("Brush type does not exist: ", brush);
 			}
+
+			tool.smartMerge();
+
+			// Reset
 			brush = undefined;
+			tool.fixedDistance = 1;
 		};
+
+		tool.smartMerge = function() {
+			debugger;
+			// Get all Path items
+			var items = paper.project.getItems({
+			    'class': Path
+			});
+			// Move down z order to first overlapping item
+			for (var i = 0; i < items.length && !items[i].intersects(finalPath); i++) { continue; }
+			for (; i < items.length; i++) {
+				// Ignore the cursor preview and non-intersecting
+				if (items[i] === cc || items[i] === finalPath || !items[i].intersects(finalPath)) { continue; }
+				if (!items[i].getFillColor()) {
+					// Ignore overlapping a hole (need more logic for this; shape with hole has multiple fill colors)
+				} else if (items[i].getFillColor().equals(finalPath.fillColor)) {
+					// Merge same fill color
+					var newPath = finalPath.unite(items[i]);
+					finalPath.remove();
+					items[i].remove();
+					finalPath = newPath;
+				} else {
+					break; // stop when you reach a different color. Doesn't touch paths with z index lower than last non-matching color
+					// // Erase different color
+					// var newPath = items[i].subtract(finalPath);
+					// if (newPath.children) {
+					//     for (var j = newPath.children.length - 1; j >= 0; j--) {
+					// 	    var child = newPath.children[j];
+					// 	    child.copyAttributes(newPath);
+					// 	    child.fillColor = newPath.fillColor;
+					// 	    child.insertAbove(newPath);
+					//     }
+				 //    	newPath.remove();
+					// }
+				 //    items[i].remove();
+				}
+			}
+			pg.undo.snapshot('broadbrush');
+		}
 
 		// broad brush =======================================================================
 		tool.onBroadMouseDown = function(event) {
@@ -91,9 +134,9 @@ pg.tools.broadbrush = function() {
 			tool.maxDistance = options.brushWidth;
 			if(event.event.button > 0) return;  // only first mouse button
 			
-			path = new Path();
-			path = pg.stylebar.applyActiveToolbarStyle(path);
-			path.add(event.point);
+			finalPath = new Path();
+			finalPath = pg.stylebar.applyActiveToolbarStyle(finalPath);
+			finalPath.add(event.point);
 			lastPoint = secondLastPoint = event.point;
 		};
 		
@@ -105,33 +148,33 @@ pg.tools.broadbrush = function() {
 			var step = (event.delta).normalize(options.brushWidth/2);
 
 			// Move the first point out away from the drag so that the end of the path is rounded
-			if (path.segments.length === 1) {
-				var removedPoint = path.removeSegment(0).point;
+			if (finalPath.segments.length === 1) {
+				var removedPoint = finalPath.removeSegment(0).point;
 				// Add handles to round the end caps
 				var handleVec = step.clone();
 				handleVec.length = options.brushWidth/2;
 				handleVec.angle += 90;
-				path.add(new Segment(removedPoint - step, -handleVec, handleVec));
+				finalPath.add(new Segment(removedPoint - step, -handleVec, handleVec));
 			}
 			step.angle += 90;
 			var top = event.middlePoint + step;
 			var bottom = event.middlePoint - step;
 
-			if (path.segments.length > 3) {
-				path.removeSegment(path.segments.length - 1);
-				path.removeSegment(0);
+			if (finalPath.segments.length > 3) {
+				finalPath.removeSegment(finalPath.segments.length - 1);
+				finalPath.removeSegment(0);
 			}
 			//path.selected = true;
-			path.add(top);
-			path.add(event.point + step);
-			path.insert(0, bottom);
-			path.insert(0, event.point - step);
-			if (path.segments.length === 5) {
+			finalPath.add(top);
+			finalPath.add(event.point + step);
+			finalPath.insert(0, bottom);
+			finalPath.insert(0, event.point - step);
+			if (finalPath.segments.length === 5) {
 				// Flatten is necessary to prevent smooth from getting rid of the effect
 				// of the handles on the first point.
-				path.flatten(options.brushWidth/5);
+				finalPath.flatten(options.brushWidth/5);
 			}
-			path.smooth();
+			finalPath.smooth();
 			lastPoint = event.point;
 			secondLastPoint = event.lastPoint;
 
@@ -148,7 +191,8 @@ pg.tools.broadbrush = function() {
 			}
 			// If the points are still equal, then there was no drag, so just draw a circle.
 			if (event.point.equals(lastPoint)) {
-				path = new Path.Circle({
+				finalPath.remove();
+				finalPath = new Path.Circle({
 				    center: event.point,
 				    radius: options.brushWidth/2,
 				    fillColor: pg.stylebar.getFillColor()
@@ -161,20 +205,20 @@ pg.tools.broadbrush = function() {
 
 				var top = event.point + step;
 				var bottom = event.point - step;
-				path.add(top);
-				path.insert(0, bottom);
+				finalPath.add(top);
+				finalPath.insert(0, bottom);
 
 				// Simplify before adding end cap so cap doesn't get warped
-				path.simplify(1);
+				finalPath.simplify(1);
 
 				// Add end cap
 				step.angle -= 90;
-				path.add(new Segment(event.point + step, handleVec, -handleVec));
-				path.closed = true;
+				finalPath.add(new Segment(event.point + step, handleVec, -handleVec));
+				finalPath.closed = true;
 			}
-			
-			// reset
-			tool.fixedDistance = 1;
+
+			// Resolve self-crossings
+			finalPath.resolveCrossings().reorient(true /* nonZero */, true /* clockwise */);
 
 			pg.undo.snapshot('broadbrush');
 		};
@@ -203,7 +247,7 @@ pg.tools.broadbrush = function() {
 			handleVec.length = options.brushWidth/2;
 			handleVec.angle += 90;
 
-			path = new Path();
+			var path = new Path();
 			path = pg.stylebar.applyActiveToolbarStyle(path);
 			// Add handles to round the end caps
 			path.add(new Segment(lastPoint - step, -handleVec, handleVec));
@@ -240,42 +284,6 @@ pg.tools.broadbrush = function() {
 			// Smooth the path.
 			//finalPath.simplify(2);
 
-			// reset
-			tool.fixedDistance = 1;
-
-			pg.undo.snapshot('broadbrush');
-
-			// Get all Path items
-			var items = paper.project.getItems({
-			    'class': Path
-			});
-			for (var i = 0; i < items.length; i++) {
-				// Ignore the cursor preview
-				if (items[i] === cc) { continue; }
-				if (!items[i].intersects(finalPath)) { continue; }
-				if (!items[i].getFillColor()) {
-					// Ignore overlapping a hole (need more logic for this; shape with hole has multiple fill colors)
-				} else if (items[i].getFillColor().equals(finalPath.fillColor)) {
-					// Merge same fill color
-					var newPath = finalPath.unite(items[i]);
-					finalPath.remove();
-					items[i].remove();
-					finalPath = newPath;
-				} else {
-					// Erase different color
-					var newPath = items[i].subtract(finalPath);
-					if (newPath.children) {
-					    for (var j = newPath.children.length - 1; j >= 0; j--) {
-						    var child = newPath.children[j];
-						    child.copyAttributes(newPath);
-						    child.fillColor = newPath.fillColor;
-						    child.insertAbove(newPath);
-					    }
-				    	newPath.remove();
-					}
-				    items[i].remove();
-				}
-			}
 			pg.undo.snapshot('broadbrush');
 		};
 		
