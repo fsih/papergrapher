ab46e9b2aebc38360915a1e9028bd792353573ce
DD Liu <liudi@media.mit.edu>
2017-08-07 16:02:59 -0400

    add back zoom and have panning with scroll bars

--- js/statusbar.js
+++ js/statusbar.js
@@ -7,7 +7,27 @@ pg.statusbar = function() {
 	
 	var setupZoomSelect = function() {
 		jQuery('#zoomSelect').change(function() {
+			var verticalScrollPercent = ($('#canvasContainer').scrollTop() + $('#canvasContainer').height()/2) / paper.view.viewSize.height;
+			var horizontalScrollPercent = ($('#canvasContainer').scrollLeft() + $('#canvasContainer').width()/2) / paper.view.viewSize.width;
+
+			// change paper size
+			$(paper.view.element).css("width", 500 * this.value + "px");
+			$(paper.view.element).css("height", 400 * this.value + "px");
+			paper.view.viewSize.width = 500 * this.value;
+			paper.view.viewSize.height = 400 * this.value;
 			paper.view.zoom = this.value;
+			paper.view.center = new paper.Point(0, 0);
+			if (this.value <= 1) {
+				$('#canvasContainer').css("overflow", "visible"); // Don't show scroll bars
+				$('#paperCanvas').css("position", "absolute"); // center in scroll container
+			} else {
+				$('#canvasContainer').css("overflow", "auto"); // Show scroll bars
+				$('#paperCanvas').css("position", "relative"); // 0,0 matches scroll container's 0,0
+			}
+			debugger;
+			$('#canvasContainer').scrollTop(paper.view.viewSize.height * verticalScrollPercent - $('#canvasContainer').height()/2);
+			$('#canvasContainer').scrollLeft(paper.view.viewSize.width * horizontalScrollPercent - $('#canvasContainer').width()/2);
+
 			update();
 			this.value = '';
 			this.blur();
